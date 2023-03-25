b5828591c035ba583e1e3bb1d99438d625b9d987
aziz-codes <azizisoffline@gmail.com>
2023-03-25 17:59:23 +0500

    One depth ahead

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import Home from "./Home";
 
 const App = () => {
+  let isLoggedIn = true;
   return (
-    <>
-      <div className="flex h-auto w-screen flex-row">
-        <Sidebar />
-        <div className="flex flex-col w-full">
-          <Navbar />
-          <div className="px-4">
-            <h2>Someother content</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab
-              mollitia provident nesciunt officiis cupiditate, accusantium
-              voluptate eos repellendus tempora. Numquam quaerat quas molestias
-              maxime culpa pariatur, iusto reiciendis quo veniam.
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
+    <>{isLoggedIn ? <Home /> : "Please Login to continue to your account"}</>
   );
 };
 
